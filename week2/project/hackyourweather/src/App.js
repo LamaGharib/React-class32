f18c95ecbcaeb82e633361007e95a9ba04073e13
Lama Gharib <lamagharib3@gmail.com>
2021-09-11 21:46:48 +0200

    adding a check for the loading

--- week2/project/hackyourweather/src/App.js
+++ week2/project/hackyourweather/src/App.js
@@ -6,14 +6,17 @@ function App() {
   const [city, setCity] = useState("");
   const [cityData, setCityData] = useState({});
   const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   // i dont know why my fetch is not returning a weather data i asked my classmates they have the same api but working fine, so i left the City component as it was from week1 but changed the the data source
   const getCity = async (city) => {
     try {
+      setIsLoading(true);
       const results = await fetch(
         `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
       );
       const data = await results.json();
       setCityData(data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error.message);
     }
@@ -28,8 +31,8 @@ function App() {
         getCity={getCity}
         setError={setError}
       />
-      {error && <h3>u did not inter a city name </h3>}
-
+      {error && <h3>You did not inter a city name </h3>}
+      {isLoading && <p>Loading...</p>}
       {Object.keys(cityData).length !== 0 ? (
         <City {...cityData} />
       ) : (
